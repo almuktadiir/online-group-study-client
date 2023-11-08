a70c3855215a060190551b9177292f8971318af4
almuktadiir <muktadir.j729.com>
2023-11-08 10:50:57 +0600

    single assignment added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -32,18 +32,24 @@ async function run() {
     const featuresCollection = client.db("groupStudy").collection("studyFeatures");
     const userCollection = client.db("groupStudy").collection("userCreated");
 
-    // client server
+    // home feature server
     app.get('/studyFeatures', async(req, res)=>{
         const result = await featuresCollection.find().toArray();
         res.send(result);
     })
 
-
-
     // userCollection server
     app.get('/allAssignments',async (req, res)=> {
         const result = await userCollection.find().toArray();
         res.send(result)
+    })
+
+    app.get('/allAssignments/:id', async (req, res)=>{
+      const id = req.params.id;
+      console.log(id);
+      // const query = {_id: new ObjectId(id)};
+      // const result = await userCollection.findOne(query);
+      // res.send(result);
     })
 
 
