9fed4f3fab220b47b1047e580251f09b9f803ee7
almuktadiir <muktadir.j729.com>
2023-11-09 19:38:42 +0600

    modal fix update

--- src/components/Pages/SubmittedAssignments/SubmittedAssignmentDetails.jsx
+++ src/components/Pages/SubmittedAssignments/SubmittedAssignmentDetails.jsx
@@ -30,12 +30,11 @@ const SubmittedAssignmentDetails = ({ sData }) => {
 
                 
                 <div>
-                    {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                    
                     <button className="btn bg-green-300 w-full" onClick={() => document.getElementById('my_modal_3').showModal()}>View PDF</button>
                     <dialog id="my_modal_3" className="modal">
                         <div className="modal-box">
                             <form method="dialog">
-                                {/* if there is a button in form, it will close the modal */}
                                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                             </form>
                             <Document file={pdf}>
